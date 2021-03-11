3aa6bb40f10dff53b07b1b39215b816b63b33624
yicheny <1463020554@qq.com>
2021-03-11 18:57:52 +0800

    update 登录页面

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import { Button, Input } from "./components";
+import { api, globalData } from "./base";
 
 function Login(props) {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     return (<div className='login'>
-            <Card/>
-        </div>);
+        <div className="login-card">
+            <div className="login-card-body">
+                <Input type='text' prefix='用户' onChange={setUserName}/>
+                <Input type='password' prefix='密码' onChange={setPassword}/>
+                <Button primary onClick={handleLogin}>登录</Button>
+                <div className='login-error'>
+                    {error &&  <div className="login-error-inner">{error}</div>}
+                </div>
+            </div>
+        </div>
+    </div>);
+
+    async function handleLogin(){
+        // console.log(userName, password);
+        setError(null)
+        setLoading(true);
+        try{
+            await loginRequest(userName,password);
+            setLoading(false);
+        }catch ( e ){
+            setError(e.message);
+            setLoading(false);
+        }
+    }
 }
 
 export default Login;
 
-function Card(){
-    return <div className="login-card">
-        <div className="login-card-body">
-            <Input type='text' prefix='用户'/>
-            <Input type='password' prefix='密码'/>
-            <Button primary>登录</Button>
-        </div>
-    </div>
+function loginRequest(name, pwd, encrypted = false) {
+    let data = new FormData();
+    data.append('name', name);
+    data.append('password', pwd);
+    data.append('encrypted', encrypted);
+
+    return api.post('/user/login', data).then(res => {
+        globalData.User = res.data;
+    });
 }
--- src/bizComponents/Container/Container.js
+++ src/bizComponents/Container/Container.js
@@ -4,7 +4,7 @@ import { Loader } from "../../components";
 
 export default function Container({ loading,error,children }){
     if(loading) return <div className='b-container center'><Loader fill/></div>
-    if(error) return <div className='b-container center error'><span>错误信息：{ error }</span></div>
+    if(error) return <div className='b-container center error'><span>错误信息：{ error.message }</span></div>
     return <div className='b-container'>
         {children}
     </div>
